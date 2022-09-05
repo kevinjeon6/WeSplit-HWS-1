d04fb573401125307fa581b95dd63c0f49b99c53
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-09-05 16:44:23 -0400

    Initial Commit

---
+++ WeSplit-HWS-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit-HWS-1
+//
+//  Created by Kevin Mattocks on 9/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit-HWS-1/WeSplit_HWS_1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_HWS_1App.swift
+//  WeSplit-HWS-1
+//
+//  Created by Kevin Mattocks on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_HWS_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
